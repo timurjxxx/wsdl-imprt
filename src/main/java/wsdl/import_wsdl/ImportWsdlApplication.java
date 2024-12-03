2d3949ef80c652a1b2187fa7598940b8c1996188
timurrjxxx <timurrjxxx@gmail.com>
2024-12-03 11:44:58 +0500

    Initial commit

---
+++ src/main/java/wsdl/import_wsdl/ImportWsdlApplication.java
@@ -0,0 +1,13 @@
+package wsdl.import_wsdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportWsdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportWsdlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsdl/import_wsdl/ImportWsdlApplicationTests.java
@@ -0,0 +1,13 @@
+package wsdl.import_wsdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportWsdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
